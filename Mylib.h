0acb75b66cb475c4b750e51a6e12a421735d693a
gabija4nt <115726083+gabija4nt@users.noreply.github.com>
2023-02-23 20:23:12 +0200

    Update Mylib.h

---
+++ Mylib.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+#include <limits>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::left;
+using std::right;
+using std::setw;
+using std::setprecision;
+using std::fixed;
+using std::string;
+using std::copy;
+using std::sort;
+using std::numeric_limits;
+using std::streamsize;
