00d4a5e7887b929f9564778ed24e61f624a39d75
gabija4nt <115726083+gabija4nt@users.noreply.github.com>
2023-03-04 20:19:40 +0200

    Update mix.cpp

--- mix.cpp
+++ mix.cpp
@@ -1,43 +1,58 @@
-#include "Mylib.h"
 #include "mix.h"
 
-void mix(studentas &temp, std::string read_vardas, std::string write_vardas){
-  std::vector<std::string> splited;
-  std::string eil;
-  std::string outputas="";
-  //----------------------------------------------------------------------
-  auto start = std::chrono::high_resolution_clock::now(); 
-  auto st=start;
-  
-  try {
+void mix(std::string read_vardas, std::string write_vardas) {
+    std::vector<std::string> splited;
+    std::string eil;
+    //----------------------------------------------------------------------
+    auto start = std::chrono::high_resolution_clock::now();
+    auto st = start;
     std::ifstream open_f(read_vardas);
-    std::getline(open_f, eil);
-    while (open_f){ 
-      if (!open_f.eof()) {
-          std::getline(open_f, eil);
-          splited.push_back(eil);}
-      else break;
+    std::getline(open_f, eil); //ignore first line
+    try {
+        if (!open_f) {
+            throw std::runtime_error("Tuscias ivesties failas");
+        }
+        while (open_f) {
+            if (!open_f.eof()) {
+                std::getline(open_f, eil);
+                splited.push_back(eil);
+            }
+            else break;
+        }
+        open_f.close();
     }
-    open_f.close();
+    catch (std::exception& e) {
+        std::cout << "Klaida: " << e.what() << "\n";
+        return;
+    }
 
-    if (splited.empty()) {
-      throw std::runtime_error("Įvesties failas yra tuščias!");
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> diff = end - start; // Skirtumas (s)
+    std::cout << "Failo nuskaitymas tiesiai i eiluciu vektoriu uztruko: " << diff.count() << " s\n";
+
+    start = std::chrono::high_resolution_clock::now();
+    std::string outputas = "";
+    for (std::string& a : splited) {
+        pild(a);
+        (a.compare(*splited.rbegin()) != 0) ? outputas += a + "\n" : outputas += a;
     }
-  
-    pild(temp, splited, outputas);
-  
+    splited.clear();
+    diff = std::chrono::high_resolution_clock::now() - start; // Skirtumas (s)
+    std::cout << "Eiluciu vektoriaus duomenu apdorojimas ir konvertavimas i viena eilute uztruko: " << diff.count() << " s\n";
+
+    start = std::chrono::high_resolution_clock::now();
     std::ofstream out_f(write_vardas);
-    out_f << left << setw(10) << "Vardas" << setw(15) << "Pavarde";
-    out_f << left << setw(20) << "Galutinis (Vid.)" << "Galutinis (Med.)" << endl;
-    out_f << "--------------------------------------------------------" << endl; 
-  
+
+    out_f << std::left << std::setw(10) << "Vardas" << std::setw(15) << "Pavarde";
+    out_f << std::left << std::setw(20) << "Galutinis (Vid.)" << "Galutinis (Med.)" << std::endl;
+    out_f << "--------------------------------------------------------" << std::endl;
+
     out_f << outputas;
     out_f.close();
-    
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> diff = end-start; // Skirtumas (s)
-    std::cout << "Visas sugaištas laikas: "<< diff.count() << " s\n\n";
-  } catch (const std::exception& e) {
-    std::cerr << "Klaida: " << e.what() << std::endl;
-  }
+    end = std::chrono::high_resolution_clock::now();
+    diff = end - start; // Skirtumas (s)
+    std::cout << "Failo irasymas per viena eilute uztruko: " << diff.count() << " s\n";
+    //std::cout<<outputas;
+    diff = std::chrono::high_resolution_clock::now() - st; // Skirtumas (s)
+    std::cout << "Visas sugaistas laikas: " << diff.count() << " s\n\n";
 }
