256494272423487cd4c9a15e76959dab00f4614f
gabija4nt <115726083+gabija4nt@users.noreply.github.com>
2023-03-04 20:20:15 +0200

    Update mix.h

--- mix.h
+++ mix.h
@@ -1,20 +1,4 @@
 #pragma once
-#include <string>
-#include <chrono>
-#include <fstream>
-#include <iomanip>
-#include <iostream>
-#include <sstream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <vector>
-#include <exception>
+#include "fast.h"
 
-#ifndef MIX_H
-#define MIX_H
-
-#include "Mylib.h"
-
-void mix(studentas &temp, std::string read_vardas, std::string write_vardas);
-
-#endif // MIX_H
+void mix(std::string read_vardas, std::string write_vardas);
